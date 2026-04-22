9902a2a82c88b230cc02e90b422205cf5bad2747
burka27 <bur.an.22303@bk.ru>
2026-04-22 17:56:35 +0300

    start creating class TreeNode

--- LAB1/obj/Debug/net10.0/LAB1.AssemblyInfo.cs
+++ LAB1/obj/Debug/net10.0/LAB1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LAB1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffac6615e9b4a90945981bc5d6f8101c7351a569")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d6bffc51e888e9ab56176a71ba950de48ddd56e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LAB1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LAB1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
